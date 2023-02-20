42b9e609ec1fb9bc457b2651050d088cf4215ce0
adrianjl25 <mrkaisser2000@gmail.com>
2023-02-20 12:52:29 +0100

    mas cambios

--- P1/server.js
+++ P1/server.js
@@ -1,21 +1,44 @@
 const http = require('http');
 const fs = require('fs');
 
-const PUERTO = 8080;
+const PUERTO = 8088;
+
+//-- Texto HTML de la página principal
 
 const data = fs.readFileSync('server.html','utf8');
+const pagerror = fs.readFileSync('error.html','utf8');
 
 
 const server = http.createServer((req, res)=>{
     console.log("Petición recibida!");
 
-    res.statusCode = 200;
-    res.statusMessage = "OK";
+    //-- Valores de la respuesta por defecto
+    let code = 200;
+    let code_msg = "OK";
+    let page = data;
+
+    //-- Analizar el recurso
+    //-- Construir el objeto url con la url de la solicitud
+    const url = new URL(req.url, 'http://' + req.headers['host']);
+    console.log(url.pathname);
+
+    //-- Cualquier recurso que no sea la página principal
+    //-- genera un error
+    if (url.pathname != '/') {
+        code = 404;
+        code_msg = "Not Found";
+        page = pagerror;
+    }
+
+    //-- Generar la respusta en función de las variables
+    //-- code, code_msg y page
+    res.statusCode = code;
+    res.statusMessage = code_msg;
     res.setHeader('Content-Type','text/html');
-    res.write(data);
+    res.write(page);
     res.end();
 });
 
 server.listen(PUERTO);
 
-console.log("¡Servidor de Ruman's Food!. Escuchando en puerto: " + PUERTO);
+console.log("Ejemplo 7. Escuchando en puerto: " + PUERTO);
