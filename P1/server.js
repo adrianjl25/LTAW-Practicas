36838673560ca9ff85b08c2e38255f6d43b7eda1
adrianjl25 <mrkaisser2000@gmail.com>
2023-02-27 11:17:30 +0100

    Cambiando la tienda

--- P1/server.js
+++ P1/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-const PUERTO = 8088;
+const PUERTO = 8888;
 
 //-- Texto HTML de la página principal
 
@@ -41,4 +41,4 @@ const server = http.createServer((req, res)=>{
 
 server.listen(PUERTO);
 
-console.log("Ejemplo 7. Escuchando en puerto: " + PUERTO);
+console.log("¡Abriendo mi tienda!. Escuchando en puerto: " + PUERTO);
